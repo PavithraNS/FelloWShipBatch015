c7694421149123ddcc48765e4ff568d3a2a9ed92
Pavithra <nspavi1991@gmail.com>
2021-02-01 09:34:30 +0530

    [Pavithra]Add AddedWelcomeMessage

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FellowShip");
+            Console.ReadLine();
+        }
+    }
+}
